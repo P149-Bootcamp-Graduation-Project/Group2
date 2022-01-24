87f057b7285eae141cf8be45eedd6d5a382d6786
Sevil KILIÇ <skilic@ogr.iu.edu.tr>
2022-01-24 10:49:33 +0300

    uploaded

---
+++ air-producer-api/app/adapters/queue/kafka.js
@@ -0,0 +1,52 @@
+const { Kafka } = require("kafkajs");
+
+createTopic();
+async function createTopic() {
+    try {
+        // Create the client with the broker list
+        const kafka = new Kafka({
+            clientID: "kafka_start",
+            brokers: ["192.168.1.2:9092"],
+        });
+
+        // create kafka broker admin
+        const admin = kafka.admin();
+        console.log("Broker is connecting...");
+
+        // admin connection to kafka
+        await admin.connect();
+        console.log("Connection is successfully...");
+
+        // create topics and partitions
+        await admin.createTopics({
+            topics: [
+                {
+                    topic: "Temperature-sensor",
+                    numPartitions: 1,
+                },
+                {
+                    topic: "Air-sensor",
+                    numPartitions: 1,
+                },
+                {
+                    topic: "Electricity-sensor",
+                    numPartitions: 1,
+                },
+            ],
+        });
+        console.log("Topic created successfully...");
+        
+        // disconnect
+        await admin.disconnect();
+    } catch (error) {
+        console.log("[ERROR] An error occurred while creating the topic...\n");
+        console.log(error);
+
+    } finally {
+        process.exit(0);
+    }
+}
+
+module.exports = {
+    createTopic,
+};
